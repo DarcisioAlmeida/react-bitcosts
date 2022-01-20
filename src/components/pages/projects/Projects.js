a5f6ce60074560352f38ca8e0fa5dc0789d6f158
Darcisio Almeida <darcisioalmeida@gmail.com>
2022-01-20 13:43:30 -0300

    Criando Dashboard da página Projeto
    - Resgatando um projeto junto ao BD e visualizando na página
    - Criando componente Loading para aguardar o carregamento do BD (simulando)
    - Removendo Projeto do BD e da página

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -1,20 +1,88 @@
+import { useState, useEffect } from 'react'
 import {useLocation} from 'react-router-dom'
 
-import Message from '../../layout/message/Message'
+/* Import Layout */
+  import Container from '../../layout/container/Container'
+  import Loading from '../../layout/loading/Loading'
+  import LinkButton from '../../layout/linkButton/LinkButton'
+  import Message from '../../layout/message/Message'
+
+/* Import Project */
+  import ProjectCard from '../../project/projectCard/ProjectCard'
+
+import styles from './Projects.module.css'
 
 function Projects() {
+  const [projects, setProjects] = useState([])
+  const [removeLoading, setRemoveLoading] = useState(false)
+  const [projectMessage, setProjectMessage] = useState('')
+
   const location = useLocation()
   let message = ''
   if (location.state) {
     message = location.state.message
   }
 
+  useEffect(() => {
+    // Para ver o loading
+    setTimeout(
+      () =>
+        fetch('http://localhost:5000/projects', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            setProjects(data)
+            setRemoveLoading(true)
+          }),
+      100,
+    )
+  }, [])
+
+  function removeProject(id) {
+    fetch(`http://localhost:5000/projects/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(projects.filter((project) => project.id !== id))
+        setProjectMessage('Projeto removido com sucesso!')
+      })
+  }
+
   return (
-    <div>
-      <h1>Projects</h1>
-      {message && <Message type="sucess" msg={message} />}
-    </div>  
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto" />
+      </div>
+      {message && <Message type="success" msg={message} />}
+      {projectMessage && <Message type="success" msg={projectMessage} />}
+      <Container customClass="start">
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+              handleRemove={removeProject}
+            />
+          ))}
+        {!removeLoading && <Loading />}
+        {removeLoading && projects.length === 0 && (
+          <p>Não há projetos cadastrados!</p>
+        )}
+      </Container>
+    </div>
   )
-    
 }
+
 export default Projects
